188a824888542a2c43315b76859761543256ac6d
bdelapparent <benjamin.de.lapparent@gmail.com>
2019-11-08 16:48:52 +0100

    suite2 c#

--- angular-spring-boot/portal-app/src/app/add-user/add-user.component.ts
+++ angular-spring-boot/portal-app/src/app/add-user/add-user.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../model/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
-  selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css']
 })
-export class AddUserComponent implements OnInit {
+export class AddUserComponent{
 
-  constructor() { }
+  user: User = new User();
 
-  ngOnInit() {
+  constructor(private router: Router, private userService: UserService) {
+
+  }
+
+  createUser(): void {
+    this.userService.createUser(this.user)
+        .subscribe( data => {
+          alert('User created successfully.');
+        });
+
   }
 
 }
